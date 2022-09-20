96019202c0f9279147476ad21a12c7638247c95b
Keydi <keydi.garcia@vtsd.com>
2022-09-20 13:12:53 -0400

    lab03

---
+++ Lab03.java
@@ -0,0 +1,34 @@
+public class Lab03 {
+    public static void main(String ars[]) {
+        int startingValue = 10000;
+        int hours = startingValue / 3600;
+        int seconds = startingValue % 3600;
+        int minutes = seconds / 60;
+        seconds = startingValue % 60;
+
+        System.out.println("Starting seconds:" + startingValue);
+        System.out.println("Hours: " + hours);
+        System.out.println("Minutes: " + minutes);
+        System.out.println("seconds: " + seconds);
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+
